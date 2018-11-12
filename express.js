27a2865c6ba365043b98f5331593919da50f14d7
AsukaSong <yansongdong@gmail.com>
2018-11-12 17:30:56 +0800

    feat

--- express.js
+++ express.js
@@ -1,7 +1,14 @@
+var fs = require('fs')
 var express = require('express')
 
 var app = express()
 
 app.use(express.static('build'))
 
-app.listen(3001)
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/build/index.html')
+})
+
+app.listen(3001, () => {
+  console.log('listening localhost:3001...')
+})
